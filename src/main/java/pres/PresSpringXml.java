ce2d012d7c0cd1c67a99563fa4be137b46cfb778
Hicham Oubari <75031773+hichamob@users.noreply.github.com>
2022-03-13 13:15:20 +0100

    Update PresSpringXml.java

--- src/main/java/pres/PresSpringXml.java
+++ src/main/java/pres/PresSpringXml.java
@@ -7,9 +7,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class PresSpringXml {
     public static void main(String[] args) {
         // nous avons crée ici un objet de type ApplicationcContext
-        ApplicationContext context=new ClassPathXmlApplicationContext("spring-ioc.xml");
+        ApplicationContext ctx=new AnnotationConfigApplicationContext("dao","metier");
+        //ApplicationContext context=new ClassPathXmlApplicationContext("spring-ioc.xml");
         // nous recherchons ici sur un objet qui s'appelle metier
-        IMetier metier= (IMetier) context.getBean("metier");
+        //IMetier metier= (IMetier) context.getBean("metier");
+        IMetier metier=ctx.getBean(IMetier.class);
         // test
         System.out.println(metier.calcul());
     }
